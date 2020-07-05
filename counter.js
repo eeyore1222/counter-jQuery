74ceaae8b02c231962d8261fdd57674c95d2d101
a123f456c789 <66512956+a123f456c789@users.noreply.github.com>
2020-07-05 11:08:28 +0800

    each

--- counter.js
+++ counter.js
@@ -2,29 +2,41 @@
 $(document).ready(function () {
 
     $(window).scroll(function () {
-        // var 變數 = 資料 (joffsetget tap)
-        var offset = $("#test").offset();     // 取得元素位移資料 
-        var top = offset.top;                 //上方
-        var left = offset.left;               //左方
+        $("*").each(function (index, element) {
+            // var 變數 = 資料 (joffsetget tap)
+            var offset = $(this).offset();     // 取得元素位移資料 
+            var top = offset.top;                 //上方
+            var left = offset.left;               //左方
 
-        //console.log("元素的上方:"+top);
+            //console.log("元素的上方:"+top);
 
-        var windowTop = $(window).scrollTop();
+            var windowTop = $(window).scrollTop();
 
-        //console.log("視窗的上方:"+windowTop);
+            //console.log("視窗的上方:"+windowTop);
 
-        // 判斷式  如果發生甚麼事就... if (){}
+            var number = $(this).attr("data-counter-number");
+            var time = $(this).attr("data-counter-time");
+            var offset = $(this).attr("data-counter-offset");
 
-        if (windowTop > top) {
-            // console.log("開始執行動畫...");
-            $("#test").animate({
-            num : 999
-            }, {
-                duration: 2000,
-            step: (now) => {
-                    $("#test").text(Math.floor(now));     //文字(步驟)-去小數點
-                }
-            })
-        }
+            //console.log("數字:" + number);
+            //console.log("時間:" + time);
+            //console.log("位移:" + offset);
+
+            // 判斷式  如果發生甚麼事就... if (){}
+
+            if (windowTop > top - offset) {
+                // console.log("開始執行動畫...");
+                $(this).animate({
+                    num: number                  //數字              
+                }, {
+                    duration: parseInt(time), //時間 - duration 需要數字 - parseInt() 將字串轉為整數
+                    step: (now) => {              //步驟
+                        $(this).text(Math.floor(now));     //文字(步驟)-去小數點
+                    }
+                })
+            }
+        })
+
+
     });
 });
